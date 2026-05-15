9b1f0d37c200620c325f892d27a13551ed883660
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2026-05-15 18:50:51 +0200

    Adding a test

---
+++ plugins/hls-case-split-plugin/test/Main.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedLists   #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main
+  ( main
+  ) where
+
+import           Control.Lens                  (Prism', prism', (^.),
+                                                (^..), (^?))
+import           Data.Foldable                 (find)
+import qualified Data.Text                     as T
+import qualified Ide.Plugin.CaseSplit          as CS
+import qualified Language.LSP.Protocol.Lens    as L
+import           System.FilePath
+import           Test.Hls
+import qualified Test.Hls.FileSystem           as FS
+
+main :: IO ()
+main = defaultTestRunner tests
+
+caseSplitPlugin :: PluginTestDescriptor CS.Log
+caseSplitPlugin = mkPluginTestDescriptor CS.descriptor "case split"
+
+tests :: TestTree
+tests = testGroup
+  "case split"
+  [ codeActionTests
+  ]
+
+codeActionTests :: TestTree
+codeActionTests = testGroup
+  "code actions"
+  [ expectCodeActionsAvailable "Produces addMinimalMethodPlaceholders code actions for one instance" "T1"
+      [ "Add placeholders for all missing patterns"
+      ]
+  , goldenWithClass "Creates a placeholder for all missing patterns" "T1" "eq" $
+      getActionByTitle "Add placeholders for all missing patterns"
+  ]
+
+_CACodeAction :: Prism' (Command |? CodeAction) CodeAction
+_CACodeAction = prism' InR $ \case
+  InR action -> Just action
+  _          -> Nothing
+
+goldenWithClass :: TestName -> FilePath -> FilePath -> ([CodeAction] -> Session CodeAction) -> TestTree
+goldenWithClass title path desc findAction =
+  goldenWithHaskellDocInTmpDir def caseSplitPlugin title (mkFs $ FS.directProject (path <.> "hs")) path (desc <.> "expected") "hs" $ \doc -> do
+    _ <- waitForDiagnosticsFrom doc
+    actions <- concatMap (^.. _CACodeAction) <$> getAllCodeActions doc
+    action <- findAction actions
+    executeCodeAction action
+
+getActionByTitle :: T.Text -> [CodeAction] -> Session CodeAction
+getActionByTitle title actions = case find (\a -> a ^. L.title == title) actions of
+    Just a -> pure a
+    Nothing -> liftIO $ assertFailure $ "Action " <> show title <> " not found in " <> show [a ^. L.title | a <- actions]
+
+expectCodeActionsAvailable :: TestName -> FilePath -> [T.Text] -> TestTree
+expectCodeActionsAvailable title path actionTitles =
+  testCase title $ do
+    runSessionWithServer def caseSplitPlugin testDataDir $ do
+      doc <- openDoc (path <.> "hs") "haskell"
+      _ <- waitForDiagnosticsFrom doc
+      caResults <- getAllCodeActions doc
+      liftIO $ map (^? _CACodeAction . L.title) caResults
+        @?= expectedActions
+    where
+      expectedActions = Just <$> actionTitles
+
+testDataDir :: FilePath
+testDataDir = "plugins" </> "hls-case-split-plugin" </> "test" </> "testdata"
+
+mkFs :: [FS.FileTree] -> FS.VirtualFileTree
+mkFs = FS.mkVirtualFileTree testDataDir
